06faff19f7feaac91a77b8e9f43768db5090c046
rouz majlessi <rmajlessi@gmail.com>
2016-08-11 19:45:37 -0700

    will add clear all page and route

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -27,6 +27,12 @@ namespace BandTracker
         return View["success.cshtml"];
       };
 
+      Post["/bands/delete"] = _ => {
+        Band.DeleteAll();
+        return View["cleared.cshtml"];
+      };
+
+
       Get["/venues"] = _ => {
         List<Venue> AllVenues = Venue.GetAll();
         return View["venues.cshtml", AllVenues];
